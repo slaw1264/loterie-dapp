c8666a99712fc0afccb8c64cad51b5b6a7c9b19a
slaw1264 <107759443+slaw1264@users.noreply.github.com>
2025-11-05 18:02:43 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
