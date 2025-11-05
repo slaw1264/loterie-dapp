283c264134c5cd3e26e18c2640598d407b2a831d
slaw1264 <107759443+slaw1264@users.noreply.github.com>
2025-11-05 18:01:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // permet aux chemins relatifs de fonctionner sur Vercel
+  plugins: [react()],
+  base: './',  // chemins relatifs pour Vercel
   build: {
     outDir: 'dist',  // dossier de sortie
   },
